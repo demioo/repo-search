725f2b4feb06f22fcf6a309534a6225293d78c78
Demi Ogun <demiogun1@gmail.com>
2021-06-14 12:22:10 +0100

    Use new hooks in RepositoriesList

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,24 @@
+import React, { useState } from 'react'
+import { useActions } from '../hooks/useActions'
+import { useTypedSelector } from '../hooks/useTypedSelector'
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('')
+  const { searchRepositries } = useActions()
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  )
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    searchRepositries(term)
+  }
+
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
